efbaad1d098302dc3c44a9f5f00647c5e8a6dad3
Kethavath Roopsingh <kethavathroopsingh9010@gmail.com>
2026-05-16 14:18:38 +0530

    refactor: clean up and remove old routers folder tracking

--- routers/authRouters.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { signup, login } = require('../controllers/authController');
-
-// Defined API endpoints
-router.post('/signup', signup);
-router.post('/login', login);
-
-module.exports = router;
